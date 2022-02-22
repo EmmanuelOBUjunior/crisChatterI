92243fbb90fe2882da18d797dc73c1b8f0e719c0
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2022-02-22 11:08:49 +0000

    First upload

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ const App = () =>{
     return(
         <ChatEngine
             height = '100vh'
-            projectID = 'df655979-aafb-41dc-80bf-2a10f93367ff'
-            userName = 'crischat'
+            projectID = '8ce29838-d19b-4064-96de-09c7903145bf'
+            userName = 'Cris'
             userSecret = 'umntwana1.'
             renderChatFeed = {(chatAppProps) => <ChatFeed {...chatAppProps}/>}
         />
