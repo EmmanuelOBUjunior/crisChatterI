4d9b1f0ddf14d321406e6919de3c764a8e24d9e3
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2022-02-22 01:08:56 +0000

    render messages function done

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -2,6 +2,37 @@ import MessageForm from "./MessageForm"
 import MyMessage from "./MyMessage"
 import TheirMessage from "./TheirMessage"
 const ChatFeed = (props) =>{
+    const {chats, activeChat, messages, userName} = props;
+
+    const chat = chats && chats[activeChat];
+
+    const renderMessages = () =>{
+        const keys = Object.keys(messages);
+
+        return keys.map((keys,index) =>{
+            const message = messages[keys];
+            const lastMessageKey = index === 0 ? null : keys[index-1];
+            const isMyMessage = userName === message.sender.username;
+
+            return(
+                <div key = {`msg_${index}`} style = {{width: '100%'}}>
+                  <div className="message-block">
+                    {
+                        isMyMessage ?
+                        <MyMessage/>
+                        : <TheirMessage/>
+                    }  
+                    </div>
+                    <div className="read-receipts" style={{marginRight: isMyMessage ? '16px' : 0, marginLeft: isMyMessage ? 0 : '68px'}}>
+                        read-receipts
+                        </div>  
+                </div>
+            )
+        })
+    }
+
+    renderMessages()
+    
     return(
         <div>
             ChatFeed
